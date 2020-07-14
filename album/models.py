ca7fdb20b31f573ce738bc80d2ae2d66498c9366
Robert Nsiah <robert.kofi.nsiah@gmail.com>
2020-07-14 02:45:32 +0000

    rendered objects on page

--- album/models.py
+++ album/models.py
@@ -5,7 +5,7 @@ class Album(models.Model):
   title = models.CharField(blank=True, null = True, max_length=100)
   date =  models.DateField(auto_now_add=False)
   artistname = models.CharField(blank=True, null = True, max_length=100)
-  albumcover = models.ImageField(upload_to='media/album/images', height_field=None, width_field=None, max_length=None, null=True)
+  albumcover = models.ImageField(upload_to='media/album/images/', height_field=None, width_field=None, max_length=None, null=True)
 
 
   def __str__(self):
--- music/urls.py
+++ music/urls.py
@@ -20,14 +20,17 @@ from album import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
   
 
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+    
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
+        
 
         # For django versions before 2.0:
         # url(r'^__debug__/', include(debug_toolbar.urls)),
